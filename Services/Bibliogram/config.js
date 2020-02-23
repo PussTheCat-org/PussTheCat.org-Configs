70c5e3b48e06b9f3b125a609e0267ba14cd19e96
TheFrenchGhosty <TheFrenchGhosty@posteo.is>
2020-02-23 13:00:13 +0100

    Add some config files

---
+++ Services/Bibliogram/config.js
@@ -0,0 +1,13 @@
+/*
+	Welcome to the config file!
+	Add keys here to override values from /src/lib/constants.js. Please look at that file for override recommendations.
+	This file should hopefully never be altered upstream.
+	You must restart Bibliogram to apply these changes.
+*/
+
+module.exports = {
+	has_privacy_policy: true,
+	settings: {
+		enable_updater_page: true,
+}
+}
