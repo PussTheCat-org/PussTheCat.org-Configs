da165c1b81266fb77ac18c7c335944add359c405
TheFrenchGhosty <TheFrenchGhosty@posteo.is>
2020-05-12 13:23:50 +0200

    Nitter and Bibliogram config files updated

--- Services/Bibliogram/config.js
+++ Services/Bibliogram/config.js
@@ -1,6 +1,12 @@
 module.exports = {
         website_origin: "https://bibliogram.pussthecat.org",
         has_privacy_policy: true,
+        themes: {
+		custom: [                
+                	{file: "pussthecat.org", name: "PussTheCat.org"},
+        	],
+		default:"pussthecat.org"
+	},
         caching: {
                 ressource_cache_time: 3*60*60*1000,
         },
