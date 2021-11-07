eceb6b489b0852b035fd6e5baff47c25aaab6278
TheFrenchGhosty <TheFrenchGhosty@posteo.is>
2021-11-07 02:39:02 +0100

    Update the Bibliogram config

--- Services/Bibliogram/config.js
+++ Services/Bibliogram/config.js
@@ -1,23 +1,16 @@
 module.exports = {
         website_origin: "https://bibliogram.pussthecat.org",
         has_privacy_policy: true,
+        tor: {
+                for: {
+                        post_graphql: true,
+                }
+        },
         themes: {
                 default:"pussthecat.org"
-        },
-        caching: {
-                ressource_cache_time: 3*60*60*1000,
-                db_request_history: true,
         },
         default_user_settings: {
                 rewrite_youtube: "redirect.invidious.io",
                 rewrite_twitter: "nitter.pussthecat.org"
-        },
-        use_assistant: {
-                enabled: true,
-                assistants: [
-                        {
-                                origin: "https://bibliogram.art",
-                        },
-                ],
         },
 }
